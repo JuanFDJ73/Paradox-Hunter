dd75ac0afb3aa6cec9df03252e5e2c9fc5ff3deb
JuanFDJ73 <vavodri@gmail.com>
2025-12-03 21:09:22 -0500

    Add Run sprite sheet and associated metadata for character animations (demo)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,17 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 8f;
 
     private Rigidbody2D rb;
+    private Animator animator;
     private Vector2 moveInput;
     private bool jumpPressed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
+    // INPUT SYSTEM
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
@@ -23,15 +26,26 @@ public class PlayerMovement : MonoBehaviour
     public void OnJump(InputValue value)
     {
         if (value.isPressed)
-        {
             jumpPressed = true;
-        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
+        // Movimiento Horizontal
+        float moveX = moveInput.x;
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
 
+        // Animación: isRunning
+        bool isRunning = moveX != 0;
+        animator.SetBool("isRunning", isRunning);
+
+        // Flip del personaje
+        if (moveX != 0)
+        {
+            transform.localScale = new Vector3(moveX > 0 ? 1 : -1, 1, 1);
+        }
+
+        // Salto
         if (jumpPressed)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
